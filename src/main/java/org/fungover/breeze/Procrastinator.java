5d63e651794380c83a91f60d19f4a353c1fd41f2
Martin Blomberg <martin.blomberg@outlook.com>
2025-02-04 11:29:06 +0100

    Add pom.xml and first code with test (#3)
    * Add pom.xml and first code with test
    
    * Update test formatting and naming

---
+++ src/main/java/org/fungover/breeze/Procrastinator.java
@@ -0,0 +1,15 @@
+package org.fungover.breeze;
+
+/**
+ * A utility class for when you need to do absolutely nothing, but in a professional way.
+ */
+public class Procrastinator {
+    /**
+     * Does absolutely nothing, but promises to do it tomorrow.
+     *
+     * @return always returns "I'll do it tomorrow" because that's what procrastinators do
+     */
+    public static String doItLater() {
+        return "I'll do it tomorrow";
+    }
+}
