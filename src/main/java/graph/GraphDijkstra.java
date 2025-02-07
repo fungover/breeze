0eade24a710c2817da0a9eaab5bbca08cd0091ce
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-07 11:06:27 +0100

    Add constructor Node and Edge
    Co-authored-by: Maximillian Rubensson <maximillian.rubensson@iths.se>

---
+++ src/main/java/graph/GraphDijkstra.java
@@ -0,0 +1,17 @@
+package graph;
+
+import java.util.Collection;
+import java.util.List;
+
+public class GraphDijkstra implements Graph {
+
+    @Override
+    public Collection<Node> getNodes() {
+        return List.of();
+    }
+
+    @Override
+    public Collection<Edge> getEdges(Node node) {
+        return List.of();
+    }
+}
---
+++ src/main/java/graph/Main.java
@@ -0,0 +1,13 @@
+package graph;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Node<Integer> node0 = new Node<>(0);
+        Node<Integer> node7 = new Node<>(7);
+        Node<Integer> node1 = new Node<>(1);
+
+        Edge<Integer> edge = new Edge<>(node0, node7, 8);
+        Edge<Integer> edge1 = new Edge<>(node0, node1, 4);
+    }
+}
