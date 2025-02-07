a438aa862571d4f5f84ba41fd8f2cc74f949779f
Evgeniia Granina <evgeniia.granina@iths.se>
2025-02-07 11:58:42 +0100

    Added Cons class implementation with method definitions and Javadoc comments
    - Implemented methods: head, tail, prepend, append, isEmpty, size, map, filter
    - Provided Javadoc comments for all methods in the Cons class
    - Ensured clarity and consistency in documentation

--- src/main/java/org/fungover/breeze/collection/FList.java
+++ src/main/java/org/fungover/breeze/collection/FList.java
@@ -3,6 +3,10 @@ package org.fungover.breeze.collection;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * Abstract class representing an immutable functional list.
+ * @param <T> the type of elements in this list
+ */
 
 public abstract class FList<T> {
     /**
@@ -69,46 +73,190 @@ public abstract class FList<T> {
         return new Empty<>();
     }
 
+    /**
+     * Private static class representing an empty list.
+     * @param <T> the type of elements in this list
+     */
+
     private static class Empty<T> extends FList<T> {
 
+        /**
+         * Throws UnsupportedOperationException as the list is empty.
+         * @return nothing
+         * @throws UnsupportedOperationException always
+         */
         @Override
         public T head() {
             return null;
         }
 
+        /**
+         * Throws UnsupportedOperationException as the list is empty.
+         * @return nothing
+         * @throws UnsupportedOperationException always
+         */
         @Override
         public FList<T> tail() {
             return null;
         }
 
+        /**
+         * Returns a new list with the specified element added at the front.
+         * @param element the element to add
+         * @return a new list with the element added at the front
+         */
         @Override
         public FList<T> prepend(T element) {
             return null;
         }
 
+        /**
+         * Returns a new list with the specified element added at the end.
+         * @param element the element to add
+         * @return a new list with the element added at the end
+         */
         @Override
         public FList<T> append(T element) {
             return null;
         }
 
+        /**
+         * Checks if the list is empty.
+         * @return true as the list is empty
+         */
         @Override
         public boolean isEmpty() {
             return false;
         }
 
+        /**
+         * Returns the number of elements in the list.
+         * @return 0 as the list is empty
+         */
         @Override
         public int size() {
             return 0;
         }
 
+        /**
+         * Returns a new empty list with the function applied to each element.
+         * @param <R> the type of elements in the new list
+         * @param f the function to apply to each element
+         * @return a new empty list
+         */
         @Override
         public <R> FList<R> map(Function<T, R> f) {
             return null;
         }
 
+        /**
+         * Returns a new empty list with only the elements that satisfy the predicate.
+         * @param p the predicate to test elements
+         * @return a new empty list
+         */
         @Override
         public FList<T> filter(Predicate<T> p) {
             return null;
+        }
+    }
+
+    /**
+     * Private static class representing a non-empty list.
+     * @param <T> the type of elements in this list
+     */
+    private static class Cons<T> extends FList<T> {
+        private final T head;
+        private final FList<T> tail;
+
+        /**
+         * Constructs a new Cons with the specified head and tail.
+         * @param head the first element of the list
+         * @param tail the rest of the list
+         */
+        public Cons(T head, FList<T> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        /**
+         * Returns the first element of the list.
+         * @return the head element of the list
+         */
+        @Override
+        public T head() {
+            return head;
+        }
+
+        /**
+         * Returns a new list that is the tail of this list (i.e., this list without the first element).
+         * @return the tail of the list
+         */
+        @Override
+        public FList<T> tail() {
+            return tail;
+        }
+
+        /**
+         * Returns a new list with the specified element added at the front.
+         * @param element the element to add
+         * @return a new list with the element added at the front
+         */
+        @Override
+        public FList<T> prepend(T element) {
+            return new Cons<>(element, this);
+        }
+
+        /**
+         * Returns a new list with the specified element added at the end.
+         * @param element the element to add
+         * @return a new list with the element added at the end
+         */
+        @Override
+        public FList<T> append(T element) {
+            return new Cons<>(head, tail.append(element));
+        }
+
+        /**
+         * Checks if the list is empty.
+         * @return false as this list is not empty
+         */
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        /**
+         * Returns the number of elements in the list.
+         * @return the size of the list
+         */
+        @Override
+        public int size() {
+            return 1 + tail.size();
+        }
+
+        /**
+         * Returns a new list with the function applied to each element.
+         * @param <R> the type of elements in the new list
+         * @param f the function to apply to each element
+         * @return a new list with the function applied to each element
+         */
+        @Override
+        public <R> FList<R> map(Function<T, R> f) {
+            return new Cons<>(f.apply(head), tail.map(f));
+        }
+
+        /**
+         * Returns a new list with only the elements that satisfy the predicate.
+         * @param p the predicate to test elements
+         * @return a new list with only the elements that satisfy the predicate
+         */
+        @Override
+        public FList<T> filter(Predicate<T> p) {
+            if (p.test(head)) {
+                return new Cons<>(head, tail.filter(p));
+            } else {
+                return tail.filter(p);
+            }
         }
     }
 }
