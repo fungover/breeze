3bcb88e00268e995b02463928eb21d1ef32193e0
ArdaHalim2 <arda.halim@iths.se>
2025-02-07 15:01:26 +0100

    34 more strings util functions (#41)
    * Test capitalization of first letter, empty string and null
    
    * count how many times a substring exist in a string
    
    * convert text to snakeCase
    
    * remove all whitespace (space, tab, \n)
    
    * test if string is palindrome
    
    * Capitalize fist letter of string
    
    * count occurrences for a substring within string
    
    * convert string with space to snake_case
    
    * add convert string with space to snake_case
    
    * remove all whitespace characters fron a string
    
    * check for palindome in string
    
    * Update Strings.java on coderabbits request
    
    * typo fixes
    
    * typo fixes 2
    
    * push
    
    * snake_case logic fix
    
    * potential DoS fix
    
    * potential DoS fix 2
    
    * remove redundancy
    
    * review1 fix
    
    * review2 fix

---
+++ src/test/java/org/fungover/breeze/util/StringTest.java
@@ -0,0 +1,65 @@
+package org.fungover.breeze.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringTest {
+
+    @Test
+    void capitalize_shouldCapitalizeFirstLetter() {
+        assertThat(Strings.capitalize("hello")).isEqualTo("Hello");
+        assertThat(Strings.capitalize("java")).isEqualTo("Java");
+        assertThat(Strings.capitalize("")).isEqualTo("");
+
+        assertThat(Strings.capitalize(null)).isNull();
+    }
+
+    @Test
+    void countOccurrences_shouldCountSubstringOccurrences() {
+        assertThat(Strings.countOccurrences("banana", "a")).isEqualTo(3);
+        assertThat(Strings.countOccurrences("hello world", "l")).isEqualTo(3);
+        assertThat(Strings.countOccurrences("test", "z")).isEqualTo(0);
+        assertThat(Strings.countOccurrences(null, "a")).isEqualTo(0);
+        assertThat(Strings.countOccurrences("hello", null)).isEqualTo(0);
+        assertThat(Strings.countOccurrences("hello", "")).isEqualTo(0);
+    }
+
+    @Test
+    void toSnakeCase_shouldConvertToSnakeCase() {
+        assertThat(Strings.toSnakeCase("HelloWorld")).isEqualTo("hello_world");
+        assertThat(Strings.toSnakeCase("snakeCaseTest")).isEqualTo("snake_case_test");
+        assertThat(Strings.toSnakeCase("already_snake_case")).isEqualTo("already_snake_case");
+        assertThat(Strings.toSnakeCase("hello world!")).isEqualTo("hello_world!");
+        assertThat(Strings.toSnakeCase("   leading and trailing spaces   ")).isEqualTo("leading_and_trailing_spaces");
+        assertThat(Strings.toSnakeCase("")).isEqualTo("");
+        assertThat(Strings.toSnakeCase(null)).isNull();
+        assertThat(Strings.toSnakeCase("XMLHttpRequest")).isEqualTo("xml_http_request");
+        assertThat(Strings.toSnakeCase("Base64Encoder")).isEqualTo("base64_encoder");
+        assertThat(Strings.toSnakeCase("User123Name")).isEqualTo("user123_name");
+        assertThat(Strings.toSnakeCase("ALLCAPSSTRING")).isEqualTo("allcapsstring");
+    }
+
+    @Test
+    void removeWhitespace_shouldRemoveAllWhitespace() {
+        assertThat(Strings.removeWhitespace(" a b c ")).isEqualTo("abc");
+        assertThat(Strings.removeWhitespace("hello   world")).isEqualTo("helloworld");
+        assertThat(Strings.removeWhitespace("\tspaces \n and newlines")).isEqualTo("spacesandnewlines");
+        assertThat(Strings.removeWhitespace(null)).isNull();
+        assertThat(Strings.removeWhitespace("")).isEqualTo("");
+    }
+
+    @Test
+    void isPalindrome_shouldCheckForPalindrome() {
+        assertThat(Strings.isPalindrome("madam")).isTrue();
+        assertThat(Strings.isPalindrome("racecar")).isTrue();
+        assertThat(Strings.isPalindrome("a")).isTrue();
+        assertThat(Strings.isPalindrome("")).isTrue();
+        assertThat(Strings.isPalindrome("Madam")).isTrue();
+        assertThat(Strings.isPalindrome("RaceCar")).isTrue();
+
+        assertThat(Strings.isPalindrome(null)).isFalse();
+        assertThat(Strings.isPalindrome("hello")).isFalse();
+
+    }
+}
