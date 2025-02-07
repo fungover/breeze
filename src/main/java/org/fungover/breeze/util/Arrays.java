b23b1c9c70520cfe9c38d92be9703d39c9851d99
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-07 14:06:51 +0100

    Validate input size: Throw exceptions for size <= 0

---
+++ src/main/java/org/fungover/breeze/util/Arrays.java
@@ -0,0 +1,18 @@
+package org.fungover.breeze.util;
+
+public class Arrays {
+
+
+    private Arrays() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static <T> T[][] chunk(T[] array, int size) {
+
+        if (size <= 0)
+            throw new IllegalArgumentException("Size must be greater than 0");
+
+        return null;
+    }
+
+}
