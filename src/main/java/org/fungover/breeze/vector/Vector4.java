a0971dbac8ff3785fa25707814a995418881d2e0
Oskar Dahlström <0skar01@hotmail.se>
2025-02-07 11:02:16 +0100

    Adds constructor for Vector4

--- src/main/java/org/fungover/breeze/vector/Vector4.java
+++ src/main/java/org/fungover/breeze/vector/Vector4.java
@@ -1,4 +1,16 @@
 package org.fungover.breeze.vector;
 
 public class Vector4 {
+    public float x;
+    public float y;
+    public float z;
+    public float w;
+
+
+    public Vector4(float x, float y, float z, float w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
 }
