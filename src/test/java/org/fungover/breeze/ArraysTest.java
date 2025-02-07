e793761338e5d3314b8d365650510ce3dc486aee
liamr03 <liamr03@hotmail.com>
2025-02-07 12:12:27 +0100

    Added template for all tests

---
+++ src/test/java/org/fungover/breeze/ArraysTest.java
@@ -0,0 +1,48 @@
+package org.fungover.breeze;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArraysTest {
+    @Test
+    @DisplayName("Square Arrays")
+    void squareArrays () {
+
+    }
+
+    @Test
+    @DisplayName("Rectangular Arrays")
+    void rectArrays () {
+
+    }
+
+    @Test
+    @DisplayName("Single Row/Column Arrays")
+    void singleRowColumnArrays () {
+    
+    }
+    
+    @Test
+    @DisplayName("Empty Array")
+    void emptyArray () {
+    
+    }
+
+    @Test
+    @DisplayName("Irregular Arrays")
+    void irregularArrays () {
+    
+    }
+
+    @Test
+    @DisplayName("Large Arrays")
+    void largeArrays () {
+    
+    }
+
+    @Test
+    @DisplayName("Arrays with null element")
+    void arraysWithNullElement () {
+    
+    }
+}
