31090bfa0ed7a8b6b8e720d20a10cfe8edaea171
Oskar Dahlström <0skar01@hotmail.se>
2025-02-07 11:04:43 +0100

    Adds getters and setters

--- src/main/java/org/fungover/breeze/vector/Vector2.java
+++ src/main/java/org/fungover/breeze/vector/Vector2.java
@@ -1,11 +1,27 @@
 package org.fungover.breeze.vector;
 
 public class Vector2 {
-    public float x;
-    public float y;
+    private float x;
+    private float y;
 
     public Vector2(float x, float y) {
         this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
         this.y = y;
     }
 
