78cd35d945871ed2342dec8c35fe029155c9c767
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-07 14:06:28 +0100

    Add getEdges and mapDestinationAndWeight
    Co-authored-by: Maximillian Rubensson <maximillian.rubensson@iths.se>

--- src/main/java/graph/WeightedGraph.java
+++ src/main/java/graph/WeightedGraph.java
@@ -1,19 +1,17 @@
 package graph;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class WeightedGraph implements Graph {
     public List<Node<Integer>> listOfNodes = new ArrayList<>();
     public List<Edge<Integer>> listOfEdges = new ArrayList<>();
 
-    public WeightedGraph<T> addNode (Node<T> node) {
+    public WeightedGraph<T> addNode(Node<T> node) {
         listOfNodes.add(node);
         return this;
     }
 
-    public WeightedGraph<T> addEdge (Edge<T> edge) {
+    public WeightedGraph<T> addEdge(Edge<T> edge) {
         listOfEdges.add(edge);
         return this;
     }
@@ -24,8 +22,24 @@ public class WeightedGraph implements Graph {
     }
 
     @Override
-    public Collection<Edge<Integer>> getEdges(Node<Integer> node) {
-
-        return List.of();
+    public Collection<Edge<T>> getEdges(Node<T> node) {
+        List<Edge<T>> edges = new ArrayList<>();
+        for (Edge<T> edge : listOfEdges)
+            if (edge.getSource().equals(node)) {
+                edges.add(edge);
+            }
+        return edges;
     }
+
+    public void mapDestinationAndWeight(Node<T> node) {
+        for (Edge<T> edge : listOfEdges) {
+            if (edge.getSource().equals(node)) {
+                Node<T> destination = edge.getDestination();
+                Double edgeToDestination = edge.getWeight();
+                nodeWithEdge.put(destination, edgeToDestination);
+            }
+        }
+    }
+
+
 }
