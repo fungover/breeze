9aaf2929fac691e46e7c87ef8d8370553ab4b0d9
liamr03 <liamr03@hotmail.com>
2025-02-07 12:42:10 +0100

    Completed these tests: squareArrays, rectArrays, singleRowColumnArrays

--- src/test/java/org/fungover/breeze/ArraysTest.java
+++ src/test/java/org/fungover/breeze/ArraysTest.java
@@ -1,37 +1,65 @@
 package org.fungover.breeze;
 
+import org.fungover.breeze.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ArraysTest {
     @Test
     @DisplayName("Square Arrays")
     void squareArrays () {
+        Integer[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
 
+        Integer[][] expected = {
+                {1, 4, 7},
+                {2, 5, 8},
+                {3, 6, 9}
+        };
+
+        assertThat(Arrays.transpose(input)).isDeepEqualTo(expected);
     }
 
     @Test
     @DisplayName("Rectangular Arrays")
     void rectArrays () {
-
+        Integer[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+        Integer[][] expected = {
+                {1, 4, 7, 10},
+                {2, 5, 8, 11},
+                {3, 6, 9, 12}
+        };
+        assertThat(Arrays.transpose(input)).isDeepEqualTo(expected);
     }
 
     @Test
     @DisplayName("Single Row/Column Arrays")
-    void singleRowColumnArrays () {
-    
-    }
-    
-    @Test
-    @DisplayName("Empty Array")
-    void emptyArray () {
-    
+    void singleRowColumnArrays() {
+        Integer[][] input = {
+                {1, 2, 3},
+        };
+        Integer[][] expected = {
+                {1},
+                {2},
+                {3}
+        };
+        assertThat(Arrays.transpose(input)).isDeepEqualTo(expected);
     }
 
     @Test
     @DisplayName("Irregular Arrays")
     void irregularArrays () {
-    
+
     }
 
     @Test
