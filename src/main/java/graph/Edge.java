ef5fa79bc193615fe5c0d808871d9dfa34427d9e
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-07 11:06:27 +0100

    Add constructor Node and Edge
    Co-authored-by: Maximillian Rubensson <maximillian.rubensson@iths.se>

--- src/main/java/graph/Edge.java
+++ src/main/java/graph/Edge.java
@@ -4,4 +4,10 @@ public class Edge<T> {
     Node<T> source;
     Node<T> destination;
     double weight;
+
+    public Edge (Node<T> source,Node<T> destination, double weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
 }
--- src/main/java/graph/Node.java
+++ src/main/java/graph/Node.java
@@ -7,4 +7,35 @@ public class Node<T> {
     Map<Node<T>, Double> shortestPaths;
     Node<T> previousNode;
     double distance;
+
+    public Node(T data) {
+        this.data = data;
+        this.shortestPaths = new HashMap<>();
+        this.previousNode = null;
+        this.distance = Double.MAX_VALUE;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Map<Node<T>, Double> getShortestPaths() {
+        return shortestPaths;
+    }
+
+    public Node<T> getPreviousNode() {
+        return previousNode;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setPreviousNode(Node<T> previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
 }
