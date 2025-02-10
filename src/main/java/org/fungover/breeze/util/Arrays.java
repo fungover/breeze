afcb34d7390d83f295fbc2417913d3c496541fa9
Eva Hottinen <ehottinen@gmail.com>
2025-02-10 13:44:05 +0100

    Created weaver method

--- src/main/java/org/fungover/breeze/util/Arrays.java
+++ src/main/java/org/fungover/breeze/util/Arrays.java
@@ -2,4 +2,23 @@ package org.fungover.breeze.util;
 
 public class Arrays {
 
+    // Weaver method - Interleaves elements
+    public static <T> T[] weaver(T[] first, T[] second, T[] resultArray) {
+        if (first == null || second == null || resultArray == null) {
+            throw new IllegalArgumentException("Inputs cannot be null.");
+        }
+
+        int maxLength = first.length + second.length;
+        if (resultArray.length < maxLength) {
+            throw new IllegalArgumentException("Result array must be large enough to hold all elements.");
+        }
+
+        int i = 0, j = 0, k = 0;
+        while (i < first.length || j < second.length) {
+            if (i < first.length) resultArray[k++] = first[i++];
+            if (j < second.length) resultArray[k++] = second[j++];
+        }
+
+        return resultArray;
+    }
 }
