98610b403b222e95889b91b8228b5294bd58e385
DeenoDev00 <drizzysha@gmail.com>
2025-02-10 16:31:46 +0100

    3d commit
    Feat: Add truncate

--- src/main/java/org/fungover/breeze/StringUtils.java
+++ src/main/java/org/fungover/breeze/StringUtils.java
@@ -9,4 +9,12 @@ public class StringUtils {
     public static boolean isNullOrBlank(String str) {
         return str == null || str.trim().isEmpty();
     }
+
+    public static String truncate(String str, int maxLength, String suffix) {
+        if (str == null) return null;
+        if (str.length() <= maxLength) return str;
+        return str.substring(0, maxLength - suffix.length()) + suffix;
+    }
+
+
 }
