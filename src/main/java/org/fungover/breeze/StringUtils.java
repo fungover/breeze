994951a99b5e157c6b52453b705913a522a68e2d
DeenoDev00 <drizzysha@gmail.com>
2025-02-10 16:29:11 +0100

    2nd commit
    Feat: Add isNullOrBlank

--- src/main/java/org/fungover/breeze/StringUtils.java
+++ src/main/java/org/fungover/breeze/StringUtils.java
@@ -5,4 +5,8 @@ public class StringUtils {
     public static boolean isNullOrEmpty(String str) {
        return str == null || str.isEmpty();
     }
+
+    public static boolean isNullOrBlank(String str) {
+        return str == null || str.trim().isEmpty();
+    }
 }
