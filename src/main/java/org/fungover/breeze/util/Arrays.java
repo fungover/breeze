5e0493388389fd557b20e75bea7fad68d1e8ebae
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-10 20:13:57 +0100

    Implement chunking logic to split array into smaller chunks

--- src/main/java/org/fungover/breeze/util/Arrays.java
+++ src/main/java/org/fungover/breeze/util/Arrays.java
@@ -1,5 +1,9 @@
 package org.fungover.breeze.util;
 
+import java.lang.reflect.Array;
+
+// JavaDocs
+
 public class Arrays {
 
 
@@ -7,12 +11,26 @@ public class Arrays {
         throw new IllegalStateException("Utility class");
     }
 
+
     public static <T> T[][] chunk(T[] array, int size) {
 
         if (size <= 0)
             throw new IllegalArgumentException("Size must be greater than 0");
 
-        return null;
+
+       int numberOfChunks = (int) Math.ceil((double) array.length / size);
+       T[][] chunks = (T[][]) Array.newInstance(array.getClass().getComponentType(),numberOfChunks, 0);
+
+       for (int i = 0; i < numberOfChunks; i++) {
+           int start = i * size;
+           int length = Math.min(array.length - start, size);
+
+           T[] chunk = (T[]) Array.newInstance(array.getClass().getComponentType(), length);
+           System.arraycopy(array, start, chunk, 0, length);
+           chunks[i] = chunk;
+       }
+
+        return chunks;
     }
 
 }
