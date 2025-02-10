48847be8732483c214daf286f798ac3080a3c222
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-10 13:29:02 +0100

    Add fields and empty methods Dijkstra
    Co-authored-by: Maximillian Rubensson <maximillian.rubensson@iths.se>

--- src/main/java/graph/Dijkstra.java
+++ src/main/java/graph/Dijkstra.java
@@ -1,4 +1,32 @@
 package graph;
 
-public class Dijkstra {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dijkstra<T> {
+
+    public List<Node<T>> unvisitedNodes = new ArrayList<>();
+    public List<Node<T>> visitedNodes = new ArrayList<>();
+
+    public Dijkstra (Graph<T> graph) {
+        unvisitedNodes.addAll(graph.getNodes());
+    }
+
+    public void findShortestPath(Graph<T> graph, Node<T> start, Node<T> end) {
+
+    }
+
+   public void findAllShortestPaths(Graph<T> graph, Node<T> start){
+
+   }
+
+   public void getPath(Node<T> target) {
+
+   }
+
+   public void getDistance(Node<T> target) {
+
+   }
+
 }
+
