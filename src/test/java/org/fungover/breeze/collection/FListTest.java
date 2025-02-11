b81dfc22f90a3549bd4a99c5df5aa802c046de86
Evgeniia Granina <evgeniia.granina@iths.se>
2025-02-11 10:51:49 +0100

    Add test prepend

--- src/test/java/org/fungover/breeze/collection/FListTest.java
+++ src/test/java/org/fungover/breeze/collection/FListTest.java
@@ -1,5 +1,6 @@
 package org.fungover.breeze.collection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class FListTest {
 
     @Test
+    @DisplayName("Test Empty List")
     void testEmptyList() {
         FList<Integer> list = FList.empty();
         assertTrue(list.isEmpty());
         assertEquals(0, list.size());
         assertThrows(UnsupportedOperationException.class, list::head);
         assertThrows(UnsupportedOperationException.class, list::tail);
+    }
+
+    @Test
+    @DisplayName("Test Prepend")
+    void testPrepend() {
+        FList<Object> list = FList.empty().prepend(1);
+        assertFalse(list.isEmpty());
+        assertEquals(1, list.head());
+        assertEquals(1, list.size());
     }
 
 }
