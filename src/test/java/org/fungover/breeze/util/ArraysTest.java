cd8265b19560f265f233b5d202aa5b4c53c0ae50
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-12 13:21:18 +0100

    Add test: chunk list throws exception for negative size

--- src/test/java/org/fungover/breeze/util/ArraysTest.java
+++ src/test/java/org/fungover/breeze/util/ArraysTest.java
@@ -3,6 +3,8 @@ package org.fungover.breeze.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +22,13 @@ class ArraysTest {
         assertThat(exception.getMessage()).isEqualTo("Size must be greater than 0");
     }
 
+    @Test
+    @DisplayName("Chunk list throws exception for negative size")
+    void chunkListThrowsExceptionForNegativeSize() {
+        List<String> stringList = List.of("A","B","C");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> Arrays.chunkList(stringList, -1));
 
+        assertThat(exception.getMessage()).isEqualTo("Size must be greater than 0");
+    }
 
 }
