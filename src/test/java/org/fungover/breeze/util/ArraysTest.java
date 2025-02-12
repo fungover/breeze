77943502332a5c7c62f237761aec5ef6aed58753
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-12 13:20:23 +0100

    Add test: chunk array throws exception for negative size

--- src/test/java/org/fungover/breeze/util/ArraysTest.java
+++ src/test/java/org/fungover/breeze/util/ArraysTest.java
@@ -1,8 +1,24 @@
 package org.fungover.breeze.util;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 
 class ArraysTest {
+
+
+    @Test
+    @DisplayName("Chunk array throws exception for negative size")
+    void chunkArrayThrowsExceptionForNegativeSize() {
+        Integer[] integerArray = {1, 2, 3};
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> Arrays.chunk(integerArray, -1));
+
+        assertThat(exception.getMessage()).isEqualTo("Size must be greater than 0");
+    }
 
 
 
