9bf5210d5555314e702f4054739f9eee85bc4c7d
Minhya <anna.nguyen0221@gmail.com>
2025-02-12 18:13:00 +0100

    Initial file structure

---
+++ src/main/java/org/fungover/breeze/simd/SimdArrayOps.java
@@ -0,0 +1,22 @@
+package org.fungover.breeze.simd;
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorSpecies;
+import java.util.Arrays;
+
+/*
+Features:
+Array addition/subtraction
+Array multiplication
+Dot product
+Element-wise operations
+Bulk transformations
+
+Implementation:
+Use FloatVector from Vector API
+Support different vector sizes
+Provide both sequential and parallel versions
+Include benchmarking tools
+*/
+
+public class SimdArrayOps {
+}
