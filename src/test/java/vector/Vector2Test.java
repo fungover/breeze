888f1203a93d9f51d6493a4491048b0d6bb56290
Oskar Dahlström <0skar01@hotmail.se>
2025-02-12 09:20:57 +0100

    Adds test for basic arithmetic

--- src/test/java/vector/Vector2Test.java
+++ src/test/java/vector/Vector2Test.java
@@ -1,8 +1,79 @@
 package vector;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.fungover.breeze.vector.Vector2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
 
 class Vector2Test {
+
+
+    @Test
+    @DisplayName("Adds to vector X")
+    void addsToVectorX(){
+        Vector2 vector = new Vector2(3,3);
+        Vector2 vector2 = new Vector2(3,3);
+        vector.add(vector2);
+        assertThat(vector).extracting(Vector2::getX).isEqualTo(6.0f);
+    }
+    @Test
+    @DisplayName("Adds to vector Y")
+    void addsToVectorY(){
+        Vector2 vector = new Vector2(3,3);
+        Vector2 vector2 = new Vector2(3,3);
+        vector.add(vector2);
+        assertThat(vector).extracting(Vector2::getY).isEqualTo(6.0f);
+    }
+    @Test
+    @DisplayName("Sub from vector X")
+    void subFromVectorX(){
+        Vector2 vector = new Vector2(6,6);
+        Vector2 vector2 = new Vector2(3,3);
+        vector.sub(vector2);
+        assertThat(vector).extracting(Vector2::getX).isEqualTo(3.0f);
+    }
+
+    @Test
+    @DisplayName("Sub from vector y")
+    void subFromVectorY(){
+        Vector2 vector = new Vector2(6,6);
+        Vector2 vector2 = new Vector2(3,3);
+        vector.sub(vector2);
+        assertThat(vector).extracting(Vector2::getY).isEqualTo(3.0f);
+    }
+
+    @Test
+    @DisplayName("Mul vector X")
+    void mulVectorX(){
+        Vector2 vector = new Vector2(3,3);
+        vector.mul(2);
+        assertThat(vector).extracting(Vector2::getX).isEqualTo(6.0f);
+    }
+    @Test
+    @DisplayName("Mul vector Y")
+    void mulVectorY(){
+        Vector2 vector = new Vector2(3,3);
+        vector.mul(2);
+        assertThat(vector).extracting(Vector2::getY).isEqualTo(6.0f);
+    }
+
+    @Test
+    @DisplayName("Div vector X")
+    void divVectorX(){
+        Vector2 vector = new Vector2(6,6);
+        vector.div(2);
+        assertThat(vector).extracting(Vector2::getX).isEqualTo(3.0f);
+    }
+
+    @Test
+    @DisplayName("Div vector Y")
+    void divVectorY(){
+        Vector2 vector = new Vector2(6,6);
+        vector.div(2);
+        assertThat(vector).extracting(Vector2::getY).isEqualTo(3.0f);
+
+    }
 
 
 
