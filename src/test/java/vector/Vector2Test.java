3f2fe9814629684abce5342db6ae4f776091b47a
Oskar Dahlström <0skar01@hotmail.se>
2025-02-12 09:38:27 +0100

    Adds test for dot product

--- src/test/java/vector/Vector2Test.java
+++ src/test/java/vector/Vector2Test.java
@@ -3,6 +3,7 @@ package vector;
 import org.fungover.breeze.vector.Vector2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -11,69 +12,80 @@ class Vector2Test {
 
     @Test
     @DisplayName("Adds to vector X")
-    void addsToVectorX(){
-        Vector2 vector = new Vector2(3,3);
-        Vector2 vector2 = new Vector2(3,3);
+    void addsToVectorX() {
+        Vector2 vector = new Vector2(3, 3);
+        Vector2 vector2 = new Vector2(3, 3);
         vector.add(vector2);
         assertThat(vector).extracting(Vector2::getX).isEqualTo(6.0f);
     }
+
     @Test
     @DisplayName("Adds to vector Y")
-    void addsToVectorY(){
-        Vector2 vector = new Vector2(3,3);
-        Vector2 vector2 = new Vector2(3,3);
+    void addsToVectorY() {
+        Vector2 vector = new Vector2(3, 3);
+        Vector2 vector2 = new Vector2(3, 3);
         vector.add(vector2);
         assertThat(vector).extracting(Vector2::getY).isEqualTo(6.0f);
     }
+
     @Test
     @DisplayName("Sub from vector X")
-    void subFromVectorX(){
-        Vector2 vector = new Vector2(6,6);
-        Vector2 vector2 = new Vector2(3,3);
+    void subFromVectorX() {
+        Vector2 vector = new Vector2(6, 6);
+        Vector2 vector2 = new Vector2(3, 3);
         vector.sub(vector2);
         assertThat(vector).extracting(Vector2::getX).isEqualTo(3.0f);
     }
 
     @Test
     @DisplayName("Sub from vector y")
-    void subFromVectorY(){
-        Vector2 vector = new Vector2(6,6);
-        Vector2 vector2 = new Vector2(3,3);
+    void subFromVectorY() {
+        Vector2 vector = new Vector2(6, 6);
+        Vector2 vector2 = new Vector2(3, 3);
         vector.sub(vector2);
         assertThat(vector).extracting(Vector2::getY).isEqualTo(3.0f);
     }
 
     @Test
     @DisplayName("Mul vector X")
-    void mulVectorX(){
-        Vector2 vector = new Vector2(3,3);
+    void mulVectorX() {
+        Vector2 vector = new Vector2(3, 3);
         vector.mul(2);
         assertThat(vector).extracting(Vector2::getX).isEqualTo(6.0f);
     }
+
     @Test
     @DisplayName("Mul vector Y")
-    void mulVectorY(){
-        Vector2 vector = new Vector2(3,3);
+    void mulVectorY() {
+        Vector2 vector = new Vector2(3, 3);
         vector.mul(2);
         assertThat(vector).extracting(Vector2::getY).isEqualTo(6.0f);
     }
 
     @Test
     @DisplayName("Div vector X")
-    void divVectorX(){
-        Vector2 vector = new Vector2(6,6);
+    void divVectorX() {
+        Vector2 vector = new Vector2(6, 6);
         vector.div(2);
         assertThat(vector).extracting(Vector2::getX).isEqualTo(3.0f);
     }
 
     @Test
     @DisplayName("Div vector Y")
-    void divVectorY(){
-        Vector2 vector = new Vector2(6,6);
+    void divVectorY() {
+        Vector2 vector = new Vector2(6, 6);
         vector.div(2);
         assertThat(vector).extracting(Vector2::getY).isEqualTo(3.0f);
-
     }
+    @Test
+    @DisplayName("Correct dot product")
+    void correctDotProduct(){
+        Vector2 vector = new Vector2(3, 3);
+        Vector2 vector2 = new Vector2(3, 3);
+        assertThat(vector.dot(vector2)).isEqualTo(18.0f);
+    }
+
+
 
 
 
