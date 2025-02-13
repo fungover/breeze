e157f25a5f85eeffceddbe4f35cb283465f06ec0
MarcGust <marcus.gustavsson@iths.se>
2025-02-13 12:22:45 +0100

    Add remove method

--- src/main/java/org/fungover/breeze/fmultimap/FMultiMap.java
+++ src/main/java/org/fungover/breeze/fmultimap/FMultiMap.java
@@ -2,26 +2,49 @@ package org.fungover.breeze.fmultimap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class FMultiMap<K, V> {
     private final Map<K, FSet<V>> map;
 
+    private FMultiMap() {
+        this.map = new HashMap<>();
+    }
+
     private FMultiMap(Map<K, FSet<V>> map) {
-        this.map = new HashMap<>(map);
+        this.map = map;
     }
 
     public static <K, V> FMultiMap<K, V> empty() {
-        return new FMultiMap<>(new HashMap<>());
+        return new FMultiMap<>();
     }
 
     public FMultiMap<K, V> put(K key, V value) {
-        FSet<V> currentValues = map.getOrDefault(key, FSet.empty());
-        FSet<V> newValues = currentValues.add(value);
-
         Map<K, FSet<V>> newMap = new HashMap<>(map);
-        newMap.put(key, newValues);
+        FSet<V> currentSet = newMap.getOrDefault(key, FSet.empty());
+        newMap.put(key, currentSet.add(value));
+        return new FMultiMap<>(newMap);
+    }
+
+    public FMultiMap<K, V> remove(K key, V value) {
+        Map<K, FSet<V>> newMap = new HashMap<>(map);
+        FSet<V> currentSet = newMap.get(key);
+        if (currentSet == null || !currentSet.contains(value)) {
+            return this;
+        }
+
+        FSet<V> updatedSet = currentSet.remove(value);
+        if (updatedSet.isEmpty()) {
+            newMap.remove(key);
+        } else {
+            newMap.put(key, updatedSet);
+        }
 
         return new FMultiMap<>(newMap);
+    }
+
+    public Optional<FSet<V>> get(K key) {
+        return Optional.ofNullable(map.get(key));
     }
 
     public boolean containsKey(K key) {
@@ -30,10 +53,6 @@ public class FMultiMap<K, V> {
 
     public boolean containsEntry(K key, V value) {
         return map.containsKey(key) && map.get(key).contains(value);
-    }
-
-    public FSet<V> get(K key) {
-        return map.getOrDefault(key, FSet.empty());
     }
 
     public int getKeyCount() {
--- src/main/java/org/fungover/breeze/fmultimap/FSet.java
+++ src/main/java/org/fungover/breeze/fmultimap/FSet.java
@@ -4,46 +4,46 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class FSet<V> {
-    private final Set<V> set;
+public class FSet<T> {
+    private final Set<T> set;
 
-    private FSet(Set<V> set) {
+    private FSet() {
+        this.set = Collections.emptySet();
+    }
+
+    private FSet(Set<T> set) {
         this.set = Collections.unmodifiableSet(set);
     }
 
-    public static <V> FSet<V> empty() {
-        return new FSet<>(new HashSet<>());
+    public static <T> FSet<T> empty() {
+        return new FSet<>();
     }
 
-    public FSet<V> add(V value) {
-        Set<V> newSet = new HashSet<>(set);
-        newSet.add(value);
+    public FSet<T> add(T element) {
+        Set<T> newSet = new HashSet<>(set);
+        newSet.add(element);
         return new FSet<>(newSet);
+    }
+
+    public FSet<T> remove(T element) {
+        if (!set.contains(element)) {
+            return this;
+        }
+
+        Set<T> newSet = new HashSet<>(set);
+        newSet.remove(element);
+        return new FSet<>(newSet);
+    }
+
+    public boolean contains(T element) {
+        return set.contains(element);
     }
 
     public boolean isEmpty() {
         return set.isEmpty();
     }
 
-    public boolean contains(V value) {
-        return set.contains(value);
-    }
-
-    @Override
-    public String toString() {
-        return set.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FSet<?> fSet = (FSet<?>) o;
-        return set.equals(fSet.set);
-    }
-
-    @Override
-    public int hashCode() {
-        return set.hashCode();
+    public int size() {
+        return set.size();
     }
 }
