5126dd6d32950b3a06a6c9f6eff484d0c8ad30a4
Jesper S <Jesper.Sandklef@iths.se>
2025-02-13 20:13:15 +0100

    added FDeque class with javadocs
    added test class for class FDeque
    
    test to see if cl maven file works
    
    Fixat små fel i båda filerna
    
    Bytt remote till fungover breeze
    
    gick tillbaka och tog bort Optional
    
    test ny pull request
    
    Fixed 3 minor issues and updated javadocs
    
    Test, rebasing to see if my commits get verified
    
    Added an ssh key
    
    ssh key test
    
    ssh key test2
    
    changed to signing key test
    
    changed to signing key test2
    
    Signed commit hopefully

---
+++ src/test/java/org/fungover/breeze/util/FDequeTest.java
@@ -0,0 +1,91 @@
+package org.fungover.breeze.util;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FDequeTest {
+
+    @Test
+    void testEnqueueFront() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueFront("A");
+        assertThat(deque.peekFront()).isEqualTo("A");
+    }
+
+    @Test
+    void testEnqueueBack() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueBack("B");
+        assertThat(deque.peekBack()).isEqualTo("B");
+    }
+
+    @Test
+    void testDequeueFront() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueFront("A").enqueBack("B");
+        deque = deque.dequeFront();
+        assertThat(deque.peekFront()).isEqualTo("B");
+    }
+
+    @Test
+    void testDequeueBack() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueBack("B").enqueFront("A");
+        deque = deque.dequeBack();
+        assertThat(deque.peekBack()).isEqualTo("A");
+    }
+
+    @Test
+    void testMultipleEnqueuesAndDequeues() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueFront("A").enqueBack("B").enqueFront("C");
+        assertThat(deque.peekFront()).isEqualTo("C");
+        assertThat(deque.peekBack()).isEqualTo("B");
+
+        deque = deque.dequeFront();
+        assertThat(deque.peekFront()).isEqualTo("A");
+
+        deque = deque.dequeBack();
+        assertThat(deque.peekBack()).isEqualTo("A");
+    }
+
+    @Test
+    void testPeekFront() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueFront("A");
+        deque = deque.enqueBack("B");
+        assertThat(deque.peekFront()).isEqualTo("A");
+    }
+
+    @Test
+    void testPeekBack() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.enqueBack("B");
+        deque = deque.enqueFront("A");
+        assertThat(deque.peekBack()).isEqualTo("B");
+    }
+
+    @Test
+    void testIsEmpty() {
+        FDeque<String> deque = FDeque.create();
+        assertThat(deque.isEmpty()).isTrue();
+        deque = deque.enqueFront("A");
+        assertThat(deque.isEmpty()).isFalse();
+    }
+
+    @Test
+    void testPeekOnEmptyList() {
+        FDeque<String> deque = FDeque.create();
+        assertThat(deque.peekFront()).isNull();
+        assertThat(deque.peekBack()).isNull();
+    }
+
+    @Test
+    void testDequeueOnEmptyList() {
+        FDeque<String> deque = FDeque.create();
+        deque = deque.dequeFront();
+        assertThat(deque.isEmpty()).isTrue();
+
+        deque = deque.dequeBack();
+        assertThat(deque.isEmpty()).isTrue();
+    }
+}
