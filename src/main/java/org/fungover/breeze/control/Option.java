a7cd7ac90d45775f4b0800e6069cae322dd367d7
Linus Holmlund <Linus_Holmlund@outlook.com>
2025-02-13 14:59:01 +0100

    - Implemented some(T value) method to create a Some<T> instance. - Added a null check to prevent `null` values. - Throws `NullPointerException` if the input is `null`. - method provides a convenient way to construct `Some<T>` safely.

--- src/main/java/org/fungover/breeze/control/Option.java
+++ src/main/java/org/fungover/breeze/control/Option.java
@@ -4,11 +4,23 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 import net.bytebuddy.dynamic.scaffold.TypeInitializer;
-
 import java.util.Optional;
+
+/**
+ * A generic container type that represents an optional value.
+ * It can either contain a value (Some) or be empty (None).
+ * @param <T> the type of the contained value
+ */
+
 
 public abstract class Option<T> {
 
+    /**
+     * Creates a Some instance if the value is non-null, otherwise returns None.
+     * @param value the value to wrap
+     * @param <T> the type of the value
+     * @return an Option containing the value or None
+     */
 
     public static <T> Option<T> of(T value) {
 
@@ -16,6 +28,17 @@ public abstract class Option<T> {
 
     }
 
+
+    public static <T> Option<T> some(T value) {
+
+        if (value == null) {
+            throw new NullPointerException("Cannot create 'Some' with null");
+
+        }
+
+        return new Some<>(value);
+
+    }
 
 
 
