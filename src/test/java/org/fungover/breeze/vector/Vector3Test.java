44fbb9b404bd788c43cc2012d2d5b6bb7cabf780
Oskar Dahlström <0skar01@hotmail.se>
2025-02-13 08:56:17 +0100

    Adds test for normalization of vector3

--- src/test/java/org/fungover/breeze/vector/Vector3Test.java
+++ src/test/java/org/fungover/breeze/vector/Vector3Test.java
@@ -11,8 +11,8 @@ class Vector3Test {
     @Test
     @DisplayName("Adds to vector")
     void addsToVector() {
-        Vector3 vector = new Vector3(3, 3,3);
-        Vector3 vector3 = new Vector3(3, 3,3);
+        Vector3 vector = new Vector3(3, 3, 3);
+        Vector3 vector3 = new Vector3(3, 3, 3);
         vector.add(vector3);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(6.0f),
@@ -20,11 +20,12 @@ class Vector3Test {
                 () -> assertThat(vector.getZ()).isEqualTo(6.0f)
         );
     }
+
     @Test
     @DisplayName("Sub from vector")
     void subFromVector() {
-        Vector3 vector = new Vector3(6, 6,6);
-        Vector3 vector2 = new Vector3(3, 3,3);
+        Vector3 vector = new Vector3(6, 6, 6);
+        Vector3 vector2 = new Vector3(3, 3, 3);
         vector.sub(vector2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(3.0f),
@@ -36,7 +37,7 @@ class Vector3Test {
     @Test
     @DisplayName("Mul vector")
     void mulVector() {
-        Vector3 vector = new Vector3(3, 3,3);
+        Vector3 vector = new Vector3(3, 3, 3);
         vector.mul(2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(6.0f),
@@ -48,7 +49,7 @@ class Vector3Test {
     @Test
     @DisplayName("Div vector")
     void divVector() {
-        Vector3 vector = new Vector3(6, 6,6);
+        Vector3 vector = new Vector3(6, 6, 6);
         vector.div(2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(3.0f),
@@ -56,19 +57,20 @@ class Vector3Test {
                 () -> assertThat(vector.getZ()).isEqualTo(3.0f)
         );
     }
+
     @Test
     @DisplayName("Correct dot product")
     void correctDotProduct() {
-        Vector3 vector = new Vector3(3, 3,3);
-        Vector3 vector3 = new Vector3(3, 3,3);
+        Vector3 vector = new Vector3(3, 3, 3);
+        Vector3 vector3 = new Vector3(3, 3, 3);
         assertThat(vector.dot(vector3)).isEqualTo(27.0f);
     }
-    
+
     @Test
     @DisplayName("new vector from cross product")
-    void newVectorFromCrossProduct(){
-        Vector3 vector = new Vector3(1, 2,3);
-        Vector3 vector2 = new Vector3(4, 5,6);
+    void newVectorFromCrossProduct() {
+        Vector3 vector = new Vector3(1, 2, 3);
+        Vector3 vector2 = new Vector3(4, 5, 6);
         var v = vector.cross(vector2);
         assertAll(
                 () -> assertThat(v.getX()).isEqualTo(-3.0f),
@@ -79,11 +81,22 @@ class Vector3Test {
 
     @Test
     @DisplayName("Correct length")
-    void correctLength(){
-        Vector3 vector = new Vector3(2, 4,4);
+    void correctLength() {
+        Vector3 vector = new Vector3(2, 4, 4);
         assertThat(vector.length()).isEqualTo(6.0f);
     }
-    
-    
+
+    @Test
+    @DisplayName("Normalize vector")
+    void normalizeVector() {
+        Vector3 vector = new Vector3(2, 3, 4);
+        var v = vector.normalize();
+        assertAll(
+                () -> assertThat(v.getX()).isEqualTo(0.37139067f),
+                () -> assertThat(v.getY()).isEqualTo(0.55708605f),
+                () -> assertThat(v.getZ()).isEqualTo(0.74278134f)
+        );
+    }
+
 
 }
