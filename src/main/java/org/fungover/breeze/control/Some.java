236ddf30da9b803274f7853a0b6718698a4c8854
Linus Holmlund <Linus_Holmlund@outlook.com>
2025-02-13 15:30:12 +0100

    - Implemented 'isEmpty() to check if the Option is None.
    - Implemented `isDefined()` to determine if the Option contains a value.
    
    - Added `get()` to retrieve the contained value, throwing an exception if None.
    - Added `getOrElse(T other)` to return the contained value or a default.
    - Added `getOrElseGet(Supplier<? extends T> supplier)` for lazy default computation.
    - Added `getOrNull()` to return the contained value or null.
    
    Added retrieval methods for Some and None for all above
    
    Added SupressWarning on 'getInstance
     in 'None'

--- src/main/java/org/fungover/breeze/control/Some.java
+++ src/main/java/org/fungover/breeze/control/Some.java
@@ -12,4 +12,57 @@ public final class Some<T> extends Option<T> {
     }
 
 
+    /**
+     * Returns true if this Option is empty (None), false otherwise.
+     *
+     * @return true if None, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Gets the contained value.
+     *
+     * @return the contained value
+     * @throws UnsupportedOperationException if called on None
+     */
+
+    @Override
+    public T get() {
+        return value;
+    }
+
+    /**
+     * @param other
+     * @return
+     */
+
+    @Override
+    public T getOrElse(T other) {
+        return value;
+    }
+
+    /**
+     * Returns the value if present, otherwise computes and returns a default value.
+     *
+     * @param supplier the supplier function to generate a default value
+     * @return the contained value or a computed default
+     */
+
+    @Override
+    public T getOrElseGet(Supplier<? extends T> supplier) {
+        return value;
+    }
+
+    /**
+     * Returns the contained value or null if None.
+     *
+     * @return the contained value or null
+     */
+    @Override
+    public T getOrNull() {
+        return value;
+    }
 }
