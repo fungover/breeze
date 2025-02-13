b04b1256d2d138ebcf0232d0b97b560b39a749fe
Oskar Dahlström <0skar01@hotmail.se>
2025-02-13 13:17:25 +0100

    refactor to use the objects variables, "this" in all methods

--- src/main/java/org/fungover/breeze/vector/Vector4.java
+++ src/main/java/org/fungover/breeze/vector/Vector4.java
@@ -45,6 +45,7 @@ public class Vector4 {
     public void setW(float w) {
         this.w = w;
     }
+
     //basic arithmetic
     public void add(Vector4 v) {
         this.x += v.x;
@@ -59,12 +60,14 @@ public class Vector4 {
         this.z -= v.z;
         this.w -= v.w;
     }
+
     public void mul(float m) {
         this.x *= m;
         this.y *= m;
         this.z *= m;
         this.w *= m;
     }
+
     public void div(float d) {
         this.x /= d;
         this.y /= d;
@@ -89,37 +92,37 @@ public class Vector4 {
     }
 
     //Distance between vectors
-    public float distance(Vector4 v1, Vector4 v2) {
-        return (float) Math.sqrt(Math.pow(v2.x -v1.x, 2) + Math.pow(v2.y - v1.y, 2) + Math.pow(v2.z - v1.z, 2) + Math.pow(v2.w - v1.w, 2));
+    public float distance(Vector4 v2) {
+        return (float) Math.sqrt(Math.pow(v2.x - this.x, 2) + Math.pow(v2.y - this.y, 2) + Math.pow(v2.z - this.z, 2) + Math.pow(v2.w - this.w, 2));
     }
 
     //Linear interpolation
-    public Vector4 linear(Vector4 v1, Vector4 v2, float t) {
-        if(t > 1)
+    public Vector4 linear(Vector4 v2, float t) {
+        if (t > 1)
             throw new IllegalArgumentException("lerp can not be larger than 1");
-        if(t< 0)
+        if (t < 0)
             throw new IllegalArgumentException("lerp can not be negative");
 
-        return new Vector4(v1.x + t * (v2.x - v1.x), v1.y + t * (v2.y - v1.y), v1.z + t * (v2.z -v1.z), v1.w + t * (v2.w - v1.w));
+        return new Vector4(this.x + t * (v2.x - this.x), this.y + t * (v2.y - this.y), this.z + t * (v2.z - this.z), this.w + t * (v2.w - this.w));
     }
 
     //Component min/max
-    public Vector4 min(Vector4 v1, Vector4 v2) {
-        return new Vector4(Math.min(v1.x ,v2.x) ,Math.min(v1.y ,v2.y), Math.min(v1.z ,v2.z),Math.min(v1.w ,v2.w) );
+    public Vector4 min(Vector4 v2) {
+        return new Vector4(Math.min(this.x, v2.x), Math.min(this.y, v2.y), Math.min(this.z, v2.z), Math.min(this.w, v2.w));
     }
 
-    public Vector4 max(Vector4 v1, Vector4 v2) {
-        return new Vector4(Math.max(v1.x ,v2.x), Math.max(v1.y, v2.y), Math.max(v1.z ,v2.z),Math.max(v1.w ,v2.w) );
+    public Vector4 max(Vector4 v2) {
+        return new Vector4(Math.max(this.x, v2.x), Math.max(this.y, v2.y), Math.max(this.z, v2.z), Math.max(this.w, v2.w));
     }
 
     //Convert to vector2 and vector3
     public Vector2 toVector2() {
         return new Vector2(this.x, this.y);
     }
+
     public Vector3 toVector3() {
         return new Vector3(this.x, this.y, this.z);
     }
-
 
 
 }
