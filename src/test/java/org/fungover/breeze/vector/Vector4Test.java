c290cb9f6840f9df81252d21fb8f486836745c8d
Oskar Dahlström <0skar01@hotmail.se>
2025-02-13 11:22:53 +0100

    Adds test for normalization of vector4

--- src/test/java/org/fungover/breeze/vector/Vector4Test.java
+++ src/test/java/org/fungover/breeze/vector/Vector4Test.java
@@ -11,72 +11,86 @@ class Vector4Test {
 
     @Test
     @DisplayName("Adds to vector")
-    void addsToVector(){
-        Vector4 vector = new Vector4(3,3,3,3);
-        Vector4 vector2 = new Vector4(3,3,3,3);
+    void addsToVector() {
+        Vector4 vector = new Vector4(3, 3, 3, 3);
+        Vector4 vector2 = new Vector4(3, 3, 3, 3);
         vector.add(vector2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(6.0f),
                 () -> assertThat(vector.getY()).isEqualTo(6.0f),
-                () -> assertThat(vector.getW()).isEqualTo(6.0f),
-                () -> assertThat(vector.getZ()).isEqualTo(6.0f)
+                () -> assertThat(vector.getZ()).isEqualTo(6.0f),
+                () -> assertThat(vector.getW()).isEqualTo(6.0f)
         );
     }
 
     @Test
     @DisplayName("Sub from vector")
-    void subFromVector(){
-        Vector4 vector = new Vector4(6,6,6,6);
-        Vector4 vector2 = new Vector4(3,3,3,3);
+    void subFromVector() {
+        Vector4 vector = new Vector4(6, 6, 6, 6);
+        Vector4 vector2 = new Vector4(3, 3, 3, 3);
         vector.sub(vector2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(3.0f),
                 () -> assertThat(vector.getY()).isEqualTo(3.0f),
-                () -> assertThat(vector.getW()).isEqualTo(3.0f),
-                () -> assertThat(vector.getZ()).isEqualTo(3.0f)
+                () -> assertThat(vector.getZ()).isEqualTo(3.0f),
+                () -> assertThat(vector.getW()).isEqualTo(3.0f)
         );
     }
 
     @Test
     @DisplayName("Mul vector")
-    void mulVector(){
-        Vector4 vector = new Vector4(3,3,3,3);
+    void mulVector() {
+        Vector4 vector = new Vector4(3, 3, 3, 3);
         vector.mul(2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(6.0f),
                 () -> assertThat(vector.getY()).isEqualTo(6.0f),
-                () -> assertThat(vector.getW()).isEqualTo(6.0f),
-                () -> assertThat(vector.getZ()).isEqualTo(6.0f)
+                () -> assertThat(vector.getZ()).isEqualTo(6.0f),
+                () -> assertThat(vector.getW()).isEqualTo(6.0f)
         );
     }
 
     @Test
     @DisplayName("Div vector")
-    void divVector(){
-        Vector4 vector = new Vector4(6,6,6,6);
+    void divVector() {
+        Vector4 vector = new Vector4(6, 6, 6, 6);
         vector.div(2);
         assertAll(
                 () -> assertThat(vector.getX()).isEqualTo(3.0f),
                 () -> assertThat(vector.getY()).isEqualTo(3.0f),
-                () -> assertThat(vector.getW()).isEqualTo(3.0f),
-                () -> assertThat(vector.getZ()).isEqualTo(3.0f)
+                () -> assertThat(vector.getZ()).isEqualTo(3.0f),
+                () -> assertThat(vector.getW()).isEqualTo(3.0f)
         );
     }
 
     @Test
     @DisplayName("Correct dot product")
-    void correctDotProduct(){
-        Vector4 vector = new Vector4(3,3,3,3);
-        Vector4 vector2 = new Vector4(3,3,3,3);
+    void correctDotProduct() {
+        Vector4 vector = new Vector4(3, 3, 3, 3);
+        Vector4 vector2 = new Vector4(3, 3, 3, 3);
         assertThat(vector.dot(vector2)).isEqualTo(36.0f);
     }
 
     @Test
     @DisplayName("Correct length")
-    void correctLength(){
-        Vector4 vector = new Vector4(3,3,3,3);
+    void correctLength() {
+        Vector4 vector = new Vector4(3, 3, 3, 3);
         assertThat(vector.length()).isEqualTo(6.0f);
     }
+
+    @Test
+    @DisplayName("Normalize vector")
+    void normalizeVector() {
+        Vector4 vector = new Vector4(3, 4, 5, 6);
+        var v = vector.normalize();
+        assertAll(
+                () -> assertThat(v.getX()).isEqualTo(0.3234983f),
+                () -> assertThat(v.getY()).isEqualTo(0.4313311f),
+                () -> assertThat(v.getZ()).isEqualTo(0.5391638f),
+                () -> assertThat(v.getW()).isEqualTo(0.6469966f)
+        );
+    }
+
 
 
 }
