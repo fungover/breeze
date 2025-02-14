67e064790f5bf0fa1da2bdfc96c11a69ff197e45
voffie <viktorsoderborg@gmail.com>
2025-02-14 13:29:56 +0100

    feat: add implementation of interfaces

--- src/main/java/org/fungover/breeze/sql/SQL.java
+++ src/main/java/org/fungover/breeze/sql/SQL.java
@@ -1,6 +1,13 @@
 package org.fungover.breeze.sql;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SQL {
+
+  public static SelectStep select() {
+    return new SQLBuilder();
+  }
 
   public interface SelectStep {
     SelectAfterStep allColumns();
@@ -44,5 +51,93 @@ public class SQL {
 
   public interface GroupByStep {
     String build();
+  }
+
+  private static class SQLBuilder implements SelectStep, SelectAfterStep, FromStep, WhereStep, ColumnStep, EqualStep, GroupByStep {
+    private String table;
+    private final ArrayList<String> columns = new ArrayList<>();
+    private String where;
+    private String equal;
+    private String groupBy;
+
+    @Override
+    public SelectAfterStep allColumns() {
+      this.columns.add("*");
+      return this;
+    }
+
+    @Override
+    public SelectAfterStep columns(String... columns) {
+      this.columns.clear();
+      this.columns.addAll(List.of(columns));
+      return this;
+    }
+
+    @Override
+    public FromStep from(String table) {
+      this.table = table;
+      return this;
+    }
+
+    @Override
+    public WhereStep where() {
+      return this;
+    }
+
+    public GroupByStep groupBy(String column) {
+      this.groupBy = column;
+      return this;
+    }
+
+    @Override
+    public ColumnStep column(String column) {
+      this.where = column;
+      return this;
+    }
+
+    @Override
+    public EqualStep equalTo(String query) {
+      this.equal = query;
+      return this;
+    }
+
+    @Override
+    public EqualStep equalTo(boolean query) {
+      this.equal = String.valueOf(query);
+      return this;
+    }
+
+    @Override
+    public EqualStep equalTo(int query) {
+      this.equal = String.valueOf(query);
+      return this;
+    }
+
+    @Override
+    public EqualStep equalTo(float query) {
+      this.equal = String.valueOf(query);
+      return this;
+    }
+
+    @Override
+    public EqualStep equalTo(double query) {
+      this.equal = String.valueOf(query);
+      return this;
+    }
+
+    @Override
+    public String build() {
+      StringBuilder output = new StringBuilder("SELECT " + String.join(", ", columns) + " FROM " + table);
+
+      if (where != null && equal != null) {
+        output.append(!where.isEmpty() && !equal.isEmpty() ? " WHERE " + where + " = " + equal : "");
+      }
+
+      if (groupBy != null) {
+        output.append(" GROUP BY ").append(groupBy);
+      }
+
+      return output.toString();
+    }
   }
 }
