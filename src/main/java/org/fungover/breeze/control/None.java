25ca6ab3a036c09854fe20fc2324424acdae4065
Linus Holmlund <Linus_Holmlund@outlook.com>
2025-02-14 18:57:22 +0100

    Smaller alteration to contractor message for debugging purpose
    "Cannot create Some with null value. Use None instead.");
    
    -altered and added Javadoc for None.java

--- src/main/java/org/fungover/breeze/control/None.java
+++ src/main/java/org/fungover/breeze/control/None.java
@@ -7,16 +7,37 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+
+/**
+ * Represents the absence of a value in the {@link Option} type.
+ * <p>
+ * This is a singleton class that represents an empty state, similar to {@code Optional.empty()} in Java.
+ * Any operations performed on this instance will result in a no-op or return a default value.
+ * </p>
+ *
+ * @param <T> The type of the non-existent value.
+ */
 public final class None<T> extends Option<T> {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Singleton instance of {@code None}.
+     */
     private static final None<?> INSTANCE = new None<>();
 
+    /**
+     * Private constructor to enforce singleton pattern.
+     */
     private None() {}
 
-
+    /**
+     * Returns the singleton instance of {@code None}.
+     *
+     * @param <T> The type parameter.
+     * @return The singleton instance of {@code None}.
+     */
     @SuppressWarnings("unchecked")
     public static <T> None<T> getInstance() {
     return (None<T>) INSTANCE;
@@ -38,20 +59,21 @@ public final class None<T> extends Option<T> {
     }
 
     /**
-     * Returns true if this Option is empty (None), false otherwise.
+     * Always returns {@code true} as this represents an empty Option.
      *
-     * @return true if None, false otherwise
+     * @return {@code true} since this is {@code None}.
      */
     @Override
     public boolean isEmpty() {
         return true;
     }
 
+
     /**
-     * Gets the contained value.
+     * Throws an exception since {@code None} does not contain a value.
      *
-     * @return the contained value
-     * @throws UnsupportedOperationException if called on None
+     * @return never returns a value.
+     * @throws UnsupportedOperationException always.
      */
     @Override
     public T get() {
@@ -59,8 +81,10 @@ public final class None<T> extends Option<T> {
     }
 
     /**
-     * @param other
-     * @return
+     * Returns the provided alternative value since {@code None} has no value.
+     *
+     * @param other The alternative value to return.
+     * @return The provided {@code other} value.
      */
     @Override
     public T getOrElse(T other) {
@@ -68,10 +92,10 @@ public final class None<T> extends Option<T> {
     }
 
     /**
-     * Returns the value if present, otherwise computes and returns a default value.
+     * Returns a computed default value since {@code None} has no value.
      *
-     * @param supplier the supplier function to generate a default value
-     * @return the contained value or a computed default
+     * @param supplier The supplier function that provides a default value.
+     * @return The computed default value.
      */
     @Override
     public T getOrElseGet(Supplier<? extends T> supplier) {
@@ -80,9 +104,9 @@ public final class None<T> extends Option<T> {
     }
 
     /**
-     * Returns the contained value or null if None.
+     * Returns {@code null} since {@code None} has no value.
      *
-     * @return the contained value or null
+     * @return {@code null}.
      */
     @Override
     public T getOrNull() {
@@ -91,56 +115,48 @@ public final class None<T> extends Option<T> {
     }
 
     /**
-     * Throws an exception provided by the supplier since None<T> has no value.
+     * Throws an exception provided by the supplier since {@code None} has no value.
      *
-     * @param <X>               the type of the exception to be thrown
-     * @param exceptionSupplier a supplier function that provides the exception to throw
-     * @return nothing, always throws an exception
-     * @throws X the provided exception
+     * @param <X> The type of the exception to be thrown.
+     * @param exceptionSupplier The supplier function that provides the exception.
+     * @return never returns a value.
+     * @throws X the provided exception.
      */
-
     @Override
     public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
         throw exceptionSupplier.get();
     }
 
     /**
-     * Returns None since there is no value to transform.
-     * <p>
-     * The function is ignored because None<T> represents an empty state.
+     * Returns {@code None} since there is no value to transform.
      *
-     * @param <U>    the type of the would-be transformed value
-     * @param mapper a function to apply (ignored)
-     * @return None<U>, since mapping an empty Option still results in None
+     * @param <U> The type of the would-be transformed value.
+     * @param mapper A function to apply (ignored).
+     * @return {@code None<U>}.
      */
-
     @Override
     public <U> Option<U> map(Function<? super T, ? extends U> mapper) {
         return None.getInstance();
     }
 
     /**
-     * Returns None since there is no value to transform.
-     * <p>
-     * The function is ignored because None<T> represents an empty state.
+     * Returns {@code None} since there is no value to transform.
      *
-     * @param <U>    the type of the would-be transformed Option
-     * @param mapper a function that takes the contained value and returns an Option<U> (ignored)
-     * @return None<U>, since flat-mapping an empty Option still results in None
+     * @param <U> The type of the resulting Option.
+     * @param mapper A function that returns an {@code Option<U>} (ignored).
+     * @return {@code None<U>}.
      */
-
     @Override
     public <U> Option<U> flatMap(Function<? super T, Option<U>> mapper) {
         return None.getInstance();
     }
 
     /**
-     * Always returns None since there is no value to filter.
+     * Always returns {@code None} since there is no value to filter.
      *
-     * @param predicate The condition to test.
-     * @return This None instance.
+     * @param predicate The condition to test (ignored).
+     * @return This {@code None} instance.
      */
-
     @Override
     public Option<T> filter(Predicate<? super T> predicate) {
         return this;
@@ -149,35 +165,32 @@ public final class None<T> extends Option<T> {
     /**
      * Does nothing since there is no value.
      *
-     * @param action The action to perform.
+     * @param action The action to perform (ignored).
      */
-
     @Override
     public void forEach(Consumer<? super T> action) {
 
     }
 
     /**
-     * Does nothing and returns this None instance.
+     * Does nothing and returns this {@code None} instance.
      *
-     * @param action The action to perform.
-     * @return This None instance.
+     * @param action The action to perform (ignored).
+     * @return This {@code None} instance.
      */
-
     @Override
     public Option<T> peek(Consumer<? super T> action) {
         return this;
     }
 
     /**
-     * Folds the Option, returning a default value since None contains no value.
+     * Folds the Option, returning a default value since {@code None} contains no value.
      *
      * @param <U> The return type.
      * @param ifNone Supplier for the default value.
-     * @param ifPresent Function applied to the value if present.
-     * @return The default value from ifNone.
+     * @param ifPresent Function applied to the value if present (ignored).
+     * @return The default value from {@code ifNone}.
      */
-
     @Override
     public <U> U fold(Supplier<U> ifNone, Function<? super T, ? extends U> ifPresent) {
 
