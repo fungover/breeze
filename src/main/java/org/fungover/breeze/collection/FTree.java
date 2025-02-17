46b269ee8ea99d01005edbb2cad2a5a8e6fd4f74
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-02-17 13:11:16 +0100

    add EmptyTree class and generate implement methods with IDE

--- src/main/java/org/fungover/breeze/collection/FTree.java
+++ src/main/java/org/fungover/breeze/collection/FTree.java
@@ -20,4 +20,43 @@ public interface FTree<T> {
         return new EmptyTree<>();
 
     }
+
 }
+
+class EmptyTree<T> implements FTree<T> {
+
+    @Override
+
+    public T value() {
+
+        throw new UnsupportedOperationException("Empty tree has no value");
+
+    }
+
+    @Override
+    public FTree<T> left() {
+        return this;
+    }
+
+    @Override
+    public FTree<T> right() {
+        return this;
+    }
+
+    @Override
+    public FTree<T> insert(T value) {
+        return new NonEmptyTree<>(value, this, this);
+    }
+
+    @Override
+    public boolean contains(T value) {
+        return false;
+    }
+
+    @Override
+    public <R> FTree<R> map(Function<T, R> f) {
+        return new EmptyTree<>();
+    }
+}
+
+
