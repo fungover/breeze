cc141db1b7c1e9d659ce7566bdde67db616aa83d
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-02-17 14:14:36 +0100

    Add tests for TreeIsEmpty

--- src/test/java/org/fungover/breeze/collection/FTreeTest.java
+++ src/test/java/org/fungover/breeze/collection/FTreeTest.java
@@ -1,4 +1,31 @@
 package org.fungover.breeze.collection;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FTreeTest {
+
+    @Test
+    void shouldReturnFalseWhenTreeIsEmptyAndValueIsSearched() {
+        FTree<Integer> tree = FTree.empty();
+
+        assertFalse(tree.contains(10));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAccessingValueInEmptyTree() {
+        FTree<Integer> tree = FTree.empty();
+
+        assertThrows(UnsupportedOperationException.class, tree::value);
+    }
+
+    @Test
+    void shouldReturnEmptySubtreesForEmptyTree() {
+        FTree<Integer> tree = FTree.empty();
+
+        assertSame(tree.left(), tree);
+        assertSame(tree.right(), tree);
+    }
+
 }
