dac970521e58135a5fa692fa542ed8962517d8ab
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-02-17 13:03:56 +0100

    add FTree interface for immutable functional tree.
    - Defina Ftree<T> interface representing an immutable binary tree
    - Add core methods: value(), left(), right(), insert(), contains(), and map()
    - Implement static factory method empty() returning an EmptyTree instance

--- src/main/java/org/fungover/breeze/collection/FTree.java
+++ src/main/java/org/fungover/breeze/collection/FTree.java
@@ -1,4 +1,23 @@
 package org.fungover.breeze.collection;
 
-public interface FTree {
+import java.util.function.Function;
+
+public interface FTree<T> {
+
+    T value();
+
+    FTree<T> left();
+
+    FTree<T> right();
+
+    FTree<T> insert(T value);
+
+    boolean contains(T value);
+
+    <R> FTree<R> map(Function<T, R> f);
+
+    static <T> FTree<T> empty() {
+        return new EmptyTree<>();
+
+    }
 }
