68f74db9fb17e40f23362bd823326c8e809a92f0
Alexander <alexpet5888@gmail.com>
2025-02-18 14:25:14 +0100

    Refactor

--- src/test/java/org/fungover/breeze/util/RedactedTest.java
+++ src/test/java/org/fungover/breeze/util/RedactedTest.java
@@ -8,9 +8,9 @@ class RedactedTest {
 
 
     @Test
-    void redacted_Is_Able_to_Save_A_value () {
-        Redacted redacted = Redacted.make("Secret");
-        assertEquals("Secret",redacted.getValue());
+    void getValue_Should_Return_Actual_Value() {
+        Redacted ValueToBeSaved = Redacted.make("Secret");
+        assertEquals("Secret",ValueToBeSaved.getValue());
     }
 
 }
