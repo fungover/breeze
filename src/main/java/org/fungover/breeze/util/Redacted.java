6c82b3f00a6990e412303a27d82effbda00dc49f
Alexander <alexpet5888@gmail.com>
2025-02-18 13:54:45 +0100

    Initial commit

---
+++ src/main/java/org/fungover/breeze/util/Redacted.java
@@ -0,0 +1,23 @@
+package org.fungover.breeze.util;
+
+public class Redacted implements CharSequence {
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+
+}
+
+
---
+++ src/test/java/org/fungover/breeze/util/RedactedTest.java
@@ -0,0 +1,14 @@
+package org.fungover.breeze.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedactedTest {
+
+    @Test
+    void redacted_Is_Able_to_Save_A_value () {
+        Redacted redacted = new Redacted();
+    }
+
+}
