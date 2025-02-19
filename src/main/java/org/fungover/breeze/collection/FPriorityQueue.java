ff6a9bd37e869a9c7ede808969ec5554a42c6a4c
SeydaKinaci <seyda.kinaci@iths.se>
2025-02-19 18:07:25 +0100

    Add enqueue and heapifyUp methods

--- src/main/java/org/fungover/breeze/collection/FPriorityQueue.java
+++ src/main/java/org/fungover/breeze/collection/FPriorityQueue.java
@@ -14,12 +14,6 @@ public final class FPriorityQueue<T>{
      * It stores elements in a way that the highest priority element is at the root.
      */
     private final List<Node<T>> heap;
-/**
- * Inner class representing an element in the priority queue with its associated priority.
- * Each node contains an element and its priority value.
- ** @param <T> The type of element stored in the node.
- *
- */
 
 
     /**
@@ -40,17 +34,49 @@ public final class FPriorityQueue<T>{
         this.heap = heap;
     }
 
+    /**
+     * Inserts an element with a given priority into the priority queue.
+     * This method maintains immutability by creating a new list,
+     * adding the element, sorting it based on priority, and returning a new FPriorityQueue instance.
+     *
+     * @param element  The element to be added.
+     * @param priority The priority of the element (lower value indicates higher priority).
+     * @return A new FPriorityQueue instance with the element inserted in sorted order.
+     */
+    public FPriorityQueue<T> enqueue(T element, int priority ) {
+        List<Node<T>> newHeap = new ArrayList<>(heap);
+        newHeap.add(new Node<>(element, priority));
+        heapifyUp(newHeap, newHeap.size() -1);
+        return new FPriorityQueue<>(newHeap);
+    }
+
+    /**
+     * Restores the heap property by moving the element at the given index up the heap.
+     * This is done by repeatedly swapping the element with its parent until the heap property is satisfied.
+     *
+     * <p>Heap property: The parent node must have a priority lower than or equal to its children.
+     *
+     * @param heap  The list representing the heap.
+     * @param index The index of the newly added element that may violate the heap property.
+     */
+    private void heapifyUp(List<Node<T>> heap, int index) {
+        while (index>0) {
+            int parentIndex = (index - 1) / 2;
+            if (heap.get(index).priority >= heap.get(parentIndex).priority) {
+                break;
+            }
+            Collections.swap(heap, index, parentIndex);
+            index = parentIndex;
+        }
+    }
 
 
-
-
-
-
-
-
-
-
-
+    /**
+     * Inner class representing an element in the priority queue with its associated priority.
+     * Each node contains an element and its priority value.
+     ** @param <T> The type of element stored in the node.
+     *
+     */
     private static class Node<T> {
         /**
          * The element contained in the node.
