fa3c1ffb7eaff4aee6f36ad1e8d705cb5fef89bd
SeydaKinaci <seyda.kinaci@iths.se>
2025-02-19 17:27:02 +0100

    Add constructors

--- src/main/java/org/fungover/breeze/collection/FPriorityQueue.java
+++ src/main/java/org/fungover/breeze/collection/FPriorityQueue.java
@@ -8,7 +8,7 @@ import java.util.*;
  *
  * @param <T> The type of elements in the priority queue.
  */
-public class FPriorityQueue<T>{
+public final class FPriorityQueue<T>{
     /**
      * The list representing the binary heap structure.
      * It stores elements in a way that the highest priority element is at the root.
@@ -20,34 +20,57 @@ public class FPriorityQueue<T>{
  ** @param <T> The type of element stored in the node.
  *
  */
-    private static class Node<T> {
-    /**
-     * The element contained in the node.
-     */
-    final T element;
-    /**
-     * The priority of the element (lower value indicates higher priority).
-     */
-    final int priority;
 
-    /**
-     * Constructs a new node with the given element and priority.
-     *
-     * @param element  The element to be stored in the node.
-     * @param priority The priority associated with the element.
-     */
-    Node(T element, int priority) {
-        this.element = element;
-        this.priority = priority;
-    }
-}
 
     /**
      * Constructs an empty priority queue.
      * The heap is initialized as an empty list.
      */
     public FPriorityQueue() {
-        this.heap = new ArrayList<Node<T>>();
+        this.heap = new ArrayList<>();
+    }
+
+    /**
+     * Constructs a new priority queue with a given heap.
+     * This constructor is used internally to create new instances while maintaining immutability.
+     *
+     * @param heap The heap to be used for this priority queue.
+     */
+    private FPriorityQueue(List<Node<T>> heap) {
+        this.heap = heap;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private static class Node<T> {
+        /**
+         * The element contained in the node.
+         */
+        final T element;
+        /**
+         * The priority of the element (lower value indicates higher priority).
+         */
+        final int priority;
+
+        /**
+         * Constructs a new node with the given element and priority.
+         *
+         * @param element  The element to be stored in the node.
+         * @param priority The priority associated with the element.
+         */
+        Node(T element, int priority) {
+            this.element = element;
+            this.priority = priority;
+        }
     }
 
 }
