ac90fa3e16c5cd91be92ae4986d87faccc820909
Minhya <minhy96th@gmail.com>
2025-02-20 14:03:41 +0100

    added some fixes and clarifications

--- src/main/java/org/fungover/breeze/simd/SimdUtils.java
+++ src/main/java/org/fungover/breeze/simd/SimdUtils.java
@@ -1,14 +1,13 @@
 package org.fungover.breeze.simd;
 import static org.fungover.breeze.simd.SimdArrayOps.SPECIES;
 
-import java.util.Random;
 import jdk.incubator.vector.FloatVector;
 import jdk.incubator.vector.VectorMask;
 import jdk.incubator.vector.VectorOperators;
 
 public class SimdUtils {
 
-  static void processSegmentElementwise(
+  static void chunkElementwise(
       float[] arr1, float[] arr2, float[] result, int start, int end, VectorOperators.Binary op) {
     int i = start;
     // Process vectorized chunks.
@@ -18,7 +17,7 @@ public class SimdUtils {
       FloatVector res = v1.lanewise(op, v2);
       res.intoArray(result, i);
     }
-    // Process any remaining elements.
+    // Process any remaining elements with mask to fill out
     if (i < end) {
       VectorMask<Float> m = SPECIES.indexInRange(i, end);
       FloatVector v1 = FloatVector.fromArray(SPECIES, arr1, i, m);
@@ -37,16 +36,16 @@ public class SimdUtils {
    * @param end   the ending index (exclusive) of the segment.
    * @return the dot product computed for the given segment.
    */
-  static float computeDotSegment(float[] arr1, float[] arr2, int start, int end) {
+  static float dotProductForSpecies(float[] arr1, float[] arr2, int start, int end) {
     float sum = 0f;
     int i = start;
-    // Process vectorized chunks.
+    // Process chunks.
     for (; i <= end - SPECIES.length(); i += SPECIES.length()) {
       FloatVector v1 = FloatVector.fromArray(SPECIES, arr1, i);
       FloatVector v2 = FloatVector.fromArray(SPECIES, arr2, i);
       sum += v1.mul(v2).reduceLanes(VectorOperators.ADD);
     }
-    // Process any remaining elements.
+    // Process any remaining elements with mask to fill out species.
     if (i < end) {
       VectorMask<Float> m = SPECIES.indexInRange(i, end);
       FloatVector v1 = FloatVector.fromArray(SPECIES, arr1, i, m);
