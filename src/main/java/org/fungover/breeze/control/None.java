633b0815a47e81798ebd8ce9a05aa329c8a2c7f5
Linus Holmlund <Linus_holmlund@outlook.com>
2025-02-20 04:51:10 +0100

    Resolved merge conflicts in Try class, keeping public access for Try.Success & Try.Failure

--- src/main/java/org/fungover/breeze/control/None.java
+++ src/main/java/org/fungover/breeze/control/None.java
@@ -1,11 +1,14 @@
 package org.fungover.breeze.control;
 
+import com.sun.net.httpserver.Authenticator;
 import org.fungover.breeze.funclib.control.Either;
+import org.fungover.breeze.funclib.control.Try;
 
 import java.io.ObjectStreamException;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -286,4 +289,9 @@ public final class None<T extends Serializable> extends Option<T> {
         throw new UnsupportedOperationException("Cannot get value from None");
     }
 
+
+
+    public Try<T> toTry(Supplier<Exception> exceptionSupplier) {
+        return Try.failure(exceptionSupplier != null ? exceptionSupplier.get() : new NoSuchElementException("No value present"));
+    }
 }
