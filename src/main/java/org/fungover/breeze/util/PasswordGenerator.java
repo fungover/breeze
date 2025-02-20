e8c65b3f95e4372bc950a2c7b781614754660379
ArdaHalim2 <arda.halim@iths.se>
2025-02-20 11:23:05 +0100

    mplemention details

--- src/main/java/org/fungover/breeze/util/PasswordGenerator.java
+++ src/main/java/org/fungover/breeze/util/PasswordGenerator.java
@@ -1,4 +1,13 @@
 package org.fungover.breeze.util;
 
+/**
+ * PasswordGenerator is a utility class for generating secure passwords based on configurable criteria.
+ * It supports various character sets, minimum requirements, and excludes ambiguous characters.
+ */
 public class PasswordGenerator {
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String SYMBOLS = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    private static final String AMBIGUOUS = "l1O0";
 }
