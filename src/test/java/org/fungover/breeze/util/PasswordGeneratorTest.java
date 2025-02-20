6387cafe5ea777bb2f3c30c19294316770fc2045
ArdaHalim2 <arda.halim@iths.se>
2025-02-20 10:54:54 +0100

    test default password generation

--- src/test/java/org/fungover/breeze/util/PasswordGeneratorTest.java
+++ src/test/java/org/fungover/breeze/util/PasswordGeneratorTest.java
@@ -1,4 +1,16 @@
 package org.fungover.breeze.util;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class PasswordGeneratorTest {
+
+    @Test
+    public void testGenerateDefaultPassword() {
+        PasswordGenerator generator = new PasswordGenerator().builder().buld();
+        String password = generator.generate();
+        assertNotNull(password);
+        assertEquals(12, password.length());
+    }
 }
