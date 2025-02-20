dc32fd36a87a5c29451b3965de22d27af64e7a12
Minhya <minhy96th@gmail.com>
2025-02-20 15:31:03 +0100

    remove redundant check

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,3 @@
 module org.fungover.breeze {
     requires jdk.incubator.vector;
-
-    exports org.fungover.breeze.util;
-    exports org.fungover.breeze.matrix;
-    exports org.fungover.breeze.graph;
-
-    opens org.fungover.breeze.util to junit;
 }
