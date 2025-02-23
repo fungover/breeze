5dea4f85dc96ba9ad2f6c743f7037c48a311ce61
Patti88 <patricia.nilsson@iths.se>
2025-02-24 00:53:51 +0100

    tesing
    Signed-off-by: Patti88 <patricia.nilsson@iths.se>

--- src/main/java/org/fungover/breeze/control/RetryConfig.java
+++ src/main/java/org/fungover/breeze/control/RetryConfig.java
@@ -1,12 +1,11 @@
-// RetryConfig.java
 package org.fungover.breeze.control;
 
 public final class RetryConfig {
     public static final int DEFAULT_MAX_ATTEMPTS = 5;
-    public static final long DEFAULT_INITIAL_DELAY = 1000; // 1 second
-    public static final long DEFAULT_MAX_DELAY = 16000; // 16 seconds
+    public static final long DEFAULT_INITIAL_DELAY = 1000;
+    public static final long DEFAULT_MAX_DELAY = 16000;
 
-    private RetryConfig() { // Add private constructor
-        throw new AssertionError("Utility class should not be instantiated");
+    private RetryConfig() {
+        throw new AssertionError("Utility class cannot be instantiated");
     }
 }
