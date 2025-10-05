8fa25a75445dfd285e8ca754cb051e8aa864fe9e
YM <yaralievmuslim@gmail.com>
2025-10-05 16:18:34 +0200

    Fixar #140: Implementerade StringUtil.reverse(String) metod

---
+++ src/main/java/StringUtil.java
@@ -0,0 +1,9 @@
+public class StringUtil {
+
+    public static String reverse(String input) {
+        if (input == null) {
+            return null;
+        }
+        return new StringBuilder(input).reverse().toString();
+    }
+}
