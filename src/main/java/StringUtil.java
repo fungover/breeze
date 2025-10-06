b72ad71fe2900ef578d843100d3910e446a20f78
YM <yaralievmuslim@gmail.com>
2025-10-06 22:24:54 +0200

    Fixar #142: Implementerade funktionen calculateArea(length, width)

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -6,4 +6,13 @@ public class StringUtil {
         }
         return new StringBuilder(input).reverse().toString();
     }
+
+    public static double calculateArea(double length, double width) {
+        if (length <= 0 || width <= 0) {
+            return 0;
+        }
+        return length * width;
+    }
 }
+git add .
+git commit -m "Fixar #142: Implementerade funktionen calculateArea(length, width)"
